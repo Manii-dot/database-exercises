75ce3ad6dd890994e9ec6c0196e90580b532499f
Emmanuel Stephen <emmanuel.r.stephen86@gmail.com>
2021-05-18 14:10:51 -0500

    feat: fix date portion

--- albums_migration.sql
+++ albums_migration.sql
@@ -3,9 +3,9 @@ USE codeup_test_db;
 CREATE TABLE IF NOT EXISTS albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist VARCHAR(50) DEFAULT 'NONE',
-    name VARCHAR(100) NOT NULL ,
-    release_date INT UNSIGNED,
+    name VARCHAR(100) NOT NULL,
+    release_date DATE NOT NULL,
     sales FLOAT NOT NULL,
     genre VARCHAR(150) NOT NULL,
     PRIMARY KEY (id)
-);exit
+);
