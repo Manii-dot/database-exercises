96fb26ef05d2654c7607ef72aa0b94015bb266e6
Emmanuel Stephen <emmanuel.r.stephen86@gmail.com>
2021-05-18 12:39:10 -0500

    feat: initiate repo

---
+++ albums_migration.sql
@@ -0,0 +1,11 @@
+USE codeup_test_db;
+# DROP TABLE albums;
+CREATE TABLE IF NOT EXISTS albums (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(50) DEFAULT 'NONE',
+    name VARCHAR(100) NOT NULL ,
+    release_date INT UNSIGNED,
+    sales FLOAT NOT NULL,
+    genre VARCHAR(150) NOT NULL,
+    PRIMARY KEY (id)
+);exit
