0bd6177b1e323e26dea6248da0fcf4b11b87c673
Emmanuel Stephen <emmanuel.r.stephen86@gmail.com>
2021-05-18 14:36:00 -0500

    FEAT: update table

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,11 +1,13 @@
 USE codeup_test_db;
-# DROP TABLE albums;
-CREATE TABLE IF NOT EXISTS albums (
+
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist VARCHAR(50) DEFAULT 'NONE',
+    artist VARCHAR(100) NOT NULL,
     name VARCHAR(100) NOT NULL,
-    release_date DATE NOT NULL,
-    sales FLOAT NOT NULL,
+    release_date INT UNSIGNED NOT NULL,
+    sales FLOAT(6,2) NOT NULL,
     genre VARCHAR(150) NOT NULL,
     PRIMARY KEY (id)
 );
