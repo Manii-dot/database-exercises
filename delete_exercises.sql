60d1f0d2fd4bb827e56651667dfa370552def632
Emmanuel Stephen <emmanuel.r.stephen86@gmail.com>
2021-05-19 10:08:30 -0500

    feat: finish all exercises

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -2,18 +2,18 @@ USE codeup_test_db;
 
 -- Write SELECT statements for:
 --           Albums released after 1991
-
+SELECT 'albums release after 1991'  AS 'release_date';
+SELECT name FROM albums WHERE release_date > '1991';
+DELETE name FROM albums WHERE release_date > '1991';
 
 --           Albums with the genre 'disco'
-
-
+SELECT 'albums with disco genre' AS 'genre';
+SELECT name FROM albums WHERE genre = 'disco';
+DELETE name FROM albums WHERE genre = 'disco';
 --           Albums by 'Whitney Houston' (...or maybe an artist of your choice)
-
+SELECT 'albums by Whitney Houston' AS 'artist';
+SELECT name FROM albums WHERE artist = 'Eagles';
+DELETE name FROM albums WHERE artist = 'Eagles';
 
 --           Make sure to put appropriate captions before each SELECT.
-
-
---           Convert the SELECT statements to DELETE.
-
-
 --     Use the MySQL command line client to make sure your records really were removed.
--- update_exercises.sql
+++ update_exercises.sql
@@ -6,7 +6,7 @@ SELECT * FROM albums;
 --     Make all the albums 10 times more popular (sales * 10)
 UPDATE albums
 SET sales = (sales * 10);
-
+SELECT * FROM albums;
 
 --     All albums released before 1980
 SELECT name FROM albums WHERE realease_date < '1980';
@@ -14,15 +14,15 @@ SELECT name FROM albums WHERE realease_date < '1980';
 UPDATE albums
 SET release_date = '1800'
 WHERE release_date < '1980';
-
+SELECT name FROM albums WHERE realease_date = '1980';
 
 --     All albums by Michael Jackson
 SELECT name FROM albums WHERE artist = 'Michael Jackson';
 --     Change 'Michael Jackson' to 'Peter Jackson'
 UPDATE albums
-SET artist = 'PETER JACKSON'
+SET artist = 'Peter Jackson'
 WHERE artist = 'Michael Jackson';
-
+SELECT name FROM albums WHERE artist = 'Peter Jackson';
 
 --     Add SELECT statements after each UPDATE so you can see the results of your handiwork.
 
