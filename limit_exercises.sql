d9b0702cb9e2723dae5e15561d3936b2f569803f
Emmanuel Stephen <emmanuel.r.stephen86@gmail.com>
2021-05-21 10:16:48 -0500

    feat: finish limit exercise

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -1,3 +1,20 @@
 USE employees;
 
-SELECT * FROM salaries ORDER BY salary DESC LIMIT 5 OFFSET 45;
+-- 2 List the first 10 distinct last name sorted in descending order.
+SELECT DISTINCT last_name
+FROM employees
+ORDER last_name DESC
+LIMIT 10;
+
+-- 3 Create a query to get the top 5 salaries and display just the employees number from the salaries table.
+SELECT DISTINCT emp_no
+FROM salaries
+ORDER BY salary DESC
+LIMIT 5;
+
+-- 4 Try to think of your results as batches, sets, or pages. The first five results are your first page. The five after that would be your second page, etc. Update the previous query to find the tenth page of results.
+-- *** I DONT GET THIS PART AS WELL, BUT WAS COPIED FROM THE INSTRUCTOR WHEN ANOTHER STUDENT ASKED ABOUT IT --- NEED FURTHER EXPLAINATION *** --
+SELECT DISTINCT emp_no
+FROM salaries
+ORDER BY salary DESC
+LIMIT 5 OFFSET 45;
