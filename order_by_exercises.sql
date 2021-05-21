19166ccb3dff2db279c2f205d49722c23c2d42c0
Emmanuel Stephen <emmanuel.r.stephen86@gmail.com>
2021-05-21 09:57:31 -0500

    feat: finish order exercise

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,10 +1,33 @@
 USE employees;
 
--- Modify your first query to order by first name. The first result should be Irena Reutenauer and
+-- 2 Modify your first query to order by first name. The first result should be Irena Reutenauer and
 -- the last result should be Vidya Demeyer.
-SELECT*
+SELECT first_name, last_name
 FROM employees
-ORDER BY first_name DESC, 'Irena Reutenauer' AND 'Vidya Demeyer'
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY first_name
 -- 'Irena Reutenauer'
 -- AND 'Vidya Demeyer'
 LIMIT 709;
+
+-- 3 Update the query to order by first name and then last name. The first result should now be Irena Acton and the last should be Vidya Boguraev.
+SELECT first_name, last_name
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY first_name, last_name;
+
+-- 4 Change the ORDER BY clause so that you order by last name before first name. Your first result should still be Irena Acton but now the last result should be Vidya Quittner.
+SELECT first_name, last_name
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY last_name, first_name;
+
+-- 5 Update your queries for employees with 'E' in their last name to sort the results by their employee number. Your results should not change!
+SELECT emp_no, last_name, first_name
+FROM employees
+WHERE last_name LIKE 'E%'
+ORDER BY emp_no
+LIMIT 7330;
+
+-- 6 Now reverse the sort order for both queries and compare results.
+-- ****I dont understand this question****
